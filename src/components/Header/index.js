2d53e9d6ef7939f229bdc6b07f240facbeb06ec6
Chad Cook <Charles.Cook28@gmail.com>
2021-04-10 10:28:13 -0500

    created header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="#home">Home</a>
+                <a href="#projects">Projects</a>
+                <a href="#resume">Resume</a>
+                <a href="#contact">Contact Me</a>
+            </nav>
+        </header>
+    )
+}
